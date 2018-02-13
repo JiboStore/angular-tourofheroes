a58773fd2bde229e52c162cb2ef1418a5e1ce441
Hakim Hauston <hakim.hauston@igg.com>
2018-02-13 20:01:54 +0800

    [angular] add hero service
    https://angular.io/tutorial/toh-pt4

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from './model/hero';
+import { HEROES } from './model/mock-heroes';
+
 @Injectable()
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 
 }
