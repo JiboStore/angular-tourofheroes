04730f31b1c97924c04537945fedbf8b56b72f27
Hakim Hauston <hakim.hauston@gmail.com>
2018-02-17 15:49:24 +0800

    [angular] update heroes
    https://angular.io/tutorial/toh-pt6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -24,7 +27,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses, remove it when a real server is ready
+    HttpClientInMemoryWebApiModule.forRoot( InMemoryDataService, {dataEncapsulation: false} )
   ],
   providers: [
     HeroService,
