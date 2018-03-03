2a83360902e7458275cbc0b23a7c69fd422a5d34
Hakim Hauston <hakim.hauston@gmail.com>
2018-03-03 15:30:12 +0800

    [angular] routing
    https://angular.io/tutorial/toh-pt5
    
    -. You added the Angular router to navigate among different components.
    -. You turned the AppComponent into a navigation shell with <a> links and a <router-outlet>.
    -. You configured the router in an AppRoutingModule
    -. You defined simple routes, a redirect route, and a parameterized route.
    -. You used the routerLink directive in anchor elements.
    -. You refactored a tightly-coupled master/detail view into a routed detail view.
    -. You used router link parameters to navigate to the detail view of a user-selected hero.
    -. You shared the HeroService among multiple components.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,7 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
+  // selectedHero: Hero;
 
   heroes: Hero[];
 
@@ -18,9 +18,9 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe( heroes => this.heroes = heroes );
